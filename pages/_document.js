a9ec1965d8cafa509aa5dc64d03b18b489e3b695
Marcus Tan <marcus.tanchiyau@gmail.com>
2025-10-10 14:58:31 +0800

    added icon to tab

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicons */}
+        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
+        
+        {/* Theme color for mobile browsers */}
+        <meta name="theme-color" content="#1a1a1a" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
